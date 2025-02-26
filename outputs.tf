b9f4c534071d47254bf6307466cd71b264d34dac
Kobe Thuwis <kobe.thuwis@infofarm.be>
2025-02-26 12:09:52 +0100

    add more outputs to customize module

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,25 @@
-output "load_balancer_dns_name" {
-  description = "DNS name of the load balancer"
-  value       = aws_lb.lb.dns_name
+output "load_balancer" {
+  description = "Load balancer resource"
+  value       = aws_lb.lb
+}
+
+output "lb_security_group" {
+  description = "Security group for the load balancer resource"
+  value       = aws_security_group.lb_security_group
+
+}
+
+output "load_balancer_listener" {
+  description = "Load balancer listener resource"
+  value       = aws_lb_listener.lb_listener
+}
+
+output "target_group" {
+  description = "Target group resource"
+  value       = aws_lb_target_group.lb_target_group
+}
+
+output "autoscaling_group" {
+  description = "Autoscaling group resource"
+  value       = aws_autoscaling_group.autoscaling_group
 }
